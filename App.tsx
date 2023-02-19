045bd9696d95de71b840614f3d8736326c9e516d
lewatt23 <lewatt23@gmail.com>
2023-02-19 09:44:07 +0100

    chore: configure bootsplash on android

---
+++ App.tsx
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  useEffect(() => {
+    const init = async () => {};
+
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true, duration: 500}); // add animation
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+    </SafeAreaView>
+  );
+}
+
+export default App;
